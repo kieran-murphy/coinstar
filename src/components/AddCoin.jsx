aa73e39370d4f23b09b770e0c309d3cd7e185653
kmurp62rulz <kmurp62rulz@gmail.com>
2022-03-03 23:26:28 +1000

    finished tutorial with adding coins and local storage

--- src/components/AddCoin.jsx
+++ src/components/AddCoin.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { WatchListContext } from "../context/watchListContext";
 
 const AddCoin = () => {
-  return (
-    <div>AddCoin</div>
-  )
-}
+  const [isActive, setIsActive] = useState(false);
+  const { addCoin } = useContext(WatchListContext);
 
-export default AddCoin
+  const availableCoins = [
+    "bitcoin",
+    "ethereum",
+    "ripple",
+    "tether",
+    "bitcoin-cash",
+    "litecoin",
+    "eos",
+    "polkadot",
+    "cardano",
+  ];
+
+  const handleClick = (coin) => {
+    addCoin(coin);
+    setIsActive(false);
+  };
+
+  return (
+    <div className="dropdown">
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className="btn btn-primary dropdown-toggle"
+        type="button"
+      >
+        Add Coin
+      </button>
+      <div className={isActive ? "dropdown-menu show" : "dropdown-menu"}>
+        {availableCoins.map((el) => {
+          return (
+            <a
+              onClick={() => handleClick(el)}
+              href="#"
+              className="dropdown-item"
+            >
+              {el}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AddCoin;
--- src/context/watchListContext.js
+++ src/context/watchListContext.js
@@ -1,23 +1,37 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const WatchListContext = createContext();
 
 export const WatchListContextProvider = (props) => {
-  const [watchList, setWatchList] = useState([
-    "bitcoin",
-    "ethereum",
-    "ripple",
-    "litecoin",
-  ]);
+  const [watchList, setWatchList] = useState(
+    localStorage.getItem("watchList").split(",") || [
+      "bitcoin",
+      "ethereum",
+      "ripple",
+      "litecoin",
+    ]
+  );
+
+  useEffect(() => {
+    localStorage.setItem("watchList", watchList);
+  }, [watchList]);
+
+  const addCoin = (coin) => {
+    if (watchList.indexOf(coin) === -1) {
+      setWatchList([...watchList, coin]);
+    }
+  };
 
   const deleteCoin = (coin) => {
-      setWatchList(watchList.filter(el => {
-          return el !== coin
-      }))
+    setWatchList(
+      watchList.filter((el) => {
+        return el !== coin;
+      })
+    );
   };
 
   return (
-    <WatchListContext.Provider value={{ watchList, deleteCoin }}>
+    <WatchListContext.Provider value={{ watchList, deleteCoin, addCoin }}>
       {props.children}
     </WatchListContext.Provider>
   );
