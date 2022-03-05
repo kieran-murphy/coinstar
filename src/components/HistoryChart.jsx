c0831f5848cf82e42420b43f57fe32b152c05aa5
kmurp62rulz <kmurp62rulz@gmail.com>
2022-03-05 16:56:51 +1000

    fixed charts

--- src/components/HistoryChart.jsx
+++ src/components/HistoryChart.jsx
@@ -28,7 +28,7 @@ const HistoryChart = ({ data }) => {
           datasets: [
             {
               label: `${detail.name} price`,
-              data: day,
+              data: determineTimeFormat(),
               backgroundColor: "rgba(174, 305, 194, 0.5)",
               borderColor: "rgba(174, 305, 194, 0.4)",
               pointRadius: 0,
@@ -68,9 +68,24 @@ const HistoryChart = ({ data }) => {
         <canvas ref={chartRef} id="myChart" width={450} height={450}></canvas>
       </div>
       <div className="chart-button mt-1">
-        <button onClick={() => setTimeFormat("24h")} className="btn btn-outline-secondary btn-sm">24h</button>
-        <button onClick={() => setTimeFormat("7d")} className="btn btn-outline-secondary btn-sm mx-1">7d</button>
-        <button onClick={() => setTimeFormat("1y")} className="btn btn-outline-secondary btn-sm">1y</button>
+        <button
+          onClick={() => setTimeFormat("24h")}
+          className="btn btn-outline-secondary btn-sm"
+        >
+          24h
+        </button>
+        <button
+          onClick={() => setTimeFormat("7d")}
+          className="btn btn-outline-secondary btn-sm mx-1"
+        >
+          7d
+        </button>
+        <button
+          onClick={() => setTimeFormat("1y")}
+          className="btn btn-outline-secondary btn-sm"
+        >
+          1y
+        </button>
       </div>
     </div>
   );
